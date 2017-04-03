10bf6b2a33e1f748541acfc8c23cb2f0162d5de9
Mark Jones <mark@uniregistry.com>
2017-04-03 10:54:03 -0500

    Demonstrates https://github.com/react-community/create-react-native-app/issues/147

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+
+        <TouchableOpacity onPress={ (a,b,c) => { console.log(a,b,c); } }>
+          <Text>Touch Me to see what happens.</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
